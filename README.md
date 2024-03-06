4aa2ec452d5e465543006370aa04fcda2563f24d
michaeltaman <42732094+michaeltaman@users.noreply.github.com>
2024-03-06 11:06:19 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,30 @@
+### Okta-Auth0-React-App
+This is the final version integration for Okta Auth0 services for React App.
+
+
+![image](https://user-images.githubusercontent.com/30728574/216604997-691cd610-fcfc-45b8-9cae-d4887e92edb1.png)
+
+
+
+In the project directory, need run:
+
+`npm install`
+Install all the dependencies required.
+
+`npm start`
+Runs the app in the development mode.
+Open http://localhost:3000 to view it in the browser.
+
+The page will reload if make edits.
+You will also see any lint errors in the console.
+
+`npm test`
+Launches the test runner in the interactive watch mode.
+See the section about running tests for more information.
+
+`npm run build`
+Builds the app for production to the build folder.
+It correctly bundles React in production mode and optimizes the build for the best performance.
+
+The build is minified and the filenames include the hashes.
+
---
+++ src/App.tsx
@@ -0,0 +1,45 @@
+import './App.css';
+import { Navbar } from './components/navbar';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { MantineProvider, ColorSchemeProvider, ColorScheme } from '@mantine/core';
+import { useState } from 'react';
+import { Footer } from './components/footer';
+import Dashboard  from './components/dashboard';
+import { useAuth0 } from "@auth0/auth0-react";
+import { HeroText } from './components/hero';
+import Loading from './components/Loading';
+import Profile from './components/profile';
+
+function App() {
+  const { isLoading, error , isAuthenticated } = useAuth0();
+  const [colorScheme, setColorScheme] = useState<ColorScheme>('light');
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
+
+  if (error) {
+    return <div>Oops... {error.message}</div>;
+  }
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  return (
+    <Router>
+      <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+        <MantineProvider theme={{ colorScheme }} withGlobalStyles withNormalizeCSS>
+          <Navbar/>
+          <Switch>
+            <Route path="/" exact component={ isAuthenticated ? Dashboard : HeroText} />
+            <Route path="/profile" component={Profile}/>
+            <Route path="/dashboard" component={Dashboard}/>
+            <Route path='*' component={() => { return (<div>404 , PAGE NOT FOUND</div>) }}/>
+          </Switch>
+          <Footer/>
+        </MantineProvider>
+      </ColorSchemeProvider>
+    </Router>
+  );
+}
+
+export default App;
